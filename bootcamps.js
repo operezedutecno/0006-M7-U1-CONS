e8ab7a2b9341ed171a08a26bf12c3fe4bef41b58
Osman Pérez <osman2907@gmail.com>
2022-04-12 21:53:47 -0400

    Método para eliminar completado

--- bootcamps.js
+++ bootcamps.js
@@ -4,7 +4,7 @@ const pool= new Pool(
     {
         user:"postgres",
         password:"postgres",
-        database:"consolidacion",
+        database:"bootcamp0006",
         host: "localhost",
         port: 5432
     }
@@ -52,4 +52,18 @@ const actualizar = async (bootcamp) => {
 
 }
 
-module.exports={consultar, registrar, actualizar}
+const eliminar = async (id) => {
+    const config = {
+        text: "DELETE FROM bootcamps WHERE id = $1 RETURNING *",
+        values:[id]
+    }
+
+    try {
+        const respuesta= await pool.query(config);
+        return respuesta.rows
+    } catch (error) {
+        return {error: error.message};
+    }
+}
+
+module.exports={consultar, registrar, actualizar, eliminar }
